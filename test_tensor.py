e1ceaa81f887cb305634a8d9441967ac7428a90c
Wes Etheredge <wes.etheredge@protonmail.com>
2020-03-20 19:23:04 -0500

    tensor version test

--- test_tensor.py
+++ test_tensor.py
@@ -1,5 +1,8 @@
 import tensorflow as tf 
 
+def test_torch_version():
+    assert tf.VERSION >= '2.0.0'
+
 def test_gpu():
     assert len(tf.config.list_physical_devices('GPU')) != 0
 
--- test_torch.py
+++ test_torch.py
@@ -1,9 +1,8 @@
 import torch
 
-
 #version 1.4.0 on March 13th 2020
 def test_torch_version():
-    assert torch.__version__== '1.4.0'
+    assert torch.__version__ >= '1.4.0'
 
 def test_cuda():
     assert torch.cuda.is_available() == True
