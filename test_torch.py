9da3789fc9455246e2db1ea4542d9daf1109b87d
Wes Etheredge <wes.etheredge@protonmail.com>
2020-05-18 07:56:32 -0500

    Expanded tests

--- test_torch.py
+++ test_torch.py
@@ -1,12 +1,21 @@
 import torch
 
+
 #version 1.4.0 on March 13th 2020
 def test_torch_version():
     assert torch.__version__ >= '1.4.0'
 
+
 def test_cuda():
     assert torch.cuda.is_available() == True
 
+
 def test_gpu():
     assert torch.cuda.device_count() != 0 
+
+
+def test_multiple_gpu():
+    assert torch.cuda.device_count() > 1
+
+
 
